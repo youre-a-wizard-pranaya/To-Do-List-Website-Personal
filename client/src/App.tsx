0dc8d0f131bcfc2ea80e9154441e0351814286d4
Pranaya Khadgi Shahi <132862264+youre-a-wizard-pranaya@users.noreply.github.com>
2025-03-10 22:35:18 -0600

    Initial prototype for the website
    built the prototype with basic strong base, will add relevant visual components into it

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import TodoApp from "@/components/TodoApp";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={TodoApp} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <Toaster />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
